faa859ad716e49d87b042afb5f77444720c2de37
Tim Cassidy <timothy_cassidy@yahoo.ca>
2019-06-08 19:41:23 -0700

    added e2e test - currently failing

--- apps/todos-e2e/src/integration/app.spec.ts
+++ apps/todos-e2e/src/integration/app.spec.ts
@@ -7,3 +7,15 @@ describe('todos', () => {
     getGreeting().contains('Welcome to todos!');
   });
 });
+
+import { getAddTodoButton, getTodos } from '../support/app.po';
+
+describe('TodoApps', () => {
+  beforeEach(() => cy.visit('/'));
+
+  it('should display todos', () => {
+    getTodos().should(t => expect(t.length).equal(2));
+    getAddTodoButton().click();
+    getTodos().should(t => expect(t.length).equal(3));
+  });
+});
